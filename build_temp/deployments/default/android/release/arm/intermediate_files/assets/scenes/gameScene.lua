7458fe7f54741c268502d58a704ebec550a32dc3
carlossebastianloredogomez <sebas_1625@hotmail.com>
2015-09-24 15:13:18 -0500

    Intento de commit

--- build_temp/deployments/default/android/release/arm/intermediate_files/assets/scenes/gameScene.lua
+++ build_temp/deployments/default/android/release/arm/intermediate_files/assets/scenes/gameScene.lua
@@ -5,6 +5,9 @@ function crearGame()
   block = true
   local objetoSeleccionado = nil
   edicion = true
+  local joyasRecurso = 100
+  local precioCasa = 50
+  local precioComida = 25
   
 	local background=director:createSprite({
 		x=0,
@@ -13,33 +16,123 @@ function crearGame()
 		source="sprites/play/bg.png"
   })
 
-	local barra = director:createSprite({
-		x=director.displayCenterX,
-		y=-200,
-		xAnchor=0.5,
-		yAnchor=0.5,
-		source = "sprites/play/barra.png",
-    yScale = 0.6
+local background2=director:createSprite({
+		x=0,
+		y=0,
+		name="background2",
+		source="sprites/play/Ui.png"
   })
 
-	local corazon = director:createSprite({
-		x=1000,
-		y=680,
-		xAnchor=0.5,
-		yAnchor=0.5,
-		source = "sprites/play/corazon.png",
-    xScale = 0.2,
-    yScale = 0.2
+-- Animaciones
+local atlasFondoBotones = director:createAtlas({
+		width = 600,
+		height = 405,
+		numFrames = 14,
+		textureName = "sprites/menu/fondoBtn.png",
   })
+
+	local animacionFondoBotones = director:createAnimation({
+		start = 1,
+		count = 14,
+		atlas = atlasFondoBotones,
+		delay = 1/10
+		})
+	
+	local barra = director:createSprite({
+		x=-500,
+		y=director.displayCenterY,
+    xAnchor = 0.5,
+    yAnchor = 0.5,
+		name = "fondoBotones",
+		source = animacionFondoBotones,
+		xScale=1,
+		yScale=1
+  })
+
+	local atlasComida = director:createAtlas({
+		width = 68,
+		height = 109.5,
+		numFrames = 4,
+		textureName = "sprites/play/maquina.png"
+		})
+
+	local animacionComida = director:createAnimation({
+		start=1,
+		count=4,
+		atlas = atlasComida,
+		delay = 1/2
+		})
+
+	local comida = director:createSprite({
+		x=-350,
+		y=300,
+		name = "comida",
+		source = animacionComida,
+    block=false,
+    fuera = false,
+    precio = 25;
+    available = false
+  })
+
+	local atlasOxigeno = director:createAtlas({
+		width = 126.5,
+		height = 92.5,
+		numFrames = 4,
+		textureName = "sprites/play/oxigeno.png"
+		})
+
+	local animacionOxigeno = director:createAnimation({
+		start=1,
+		count=4,
+		atlas = atlasOxigeno,
+		delay = 1/2
+		})
+
+	local oxigeno = director:createSprite({
+		x=-500,
+		y=300,
+		name = "oxigeno",
+		source = animacionOxigeno,
+    block=false,
+    fuera=false,
+    precio = 25;
+    available=false
+  })
+
+	local atlasNave = director:createAtlas({
+		width = 205.5,
+		height = 114.5,
+		numFrames = 4,
+		textureName = "sprites/play/nave.png"
+		})
+
+	local animacionNave = director:createAnimation({
+		start=1,
+		count=4,
+		atlas = atlasNave,
+		delay = 1/9
+		})
+
+	local nave = director:createSprite({
+		x=205.5,
+		y=114.5,
+		name = "nave",
+		source = animacionNave,
+    block=false,
+    fuera=false,
+    available=false
+  })
+
+-- Imagenes
 
 	local construir = director:createSprite({
 		x=100,
-		y=100,
+		y=director.displayCenterY,
 		xAnchor=0.5,
 		yAnchor=0.5,
 		source = "sprites/play/flecha.png",
-    xScale = 0.2,
-    yScale = 0.2
+    xScale = -1,
+    yScale = 1
   })
 
 	local cuevaBtn = director:createSprite({
@@ -51,12 +144,7 @@ function crearGame()
 		source = "sprites/play/cuevaBtn.png"
 		})
 
-	local segundosOxigenoLabel = director:createLabel({
-		x=100,
-		y=650,
-		font = "fonts/Default.fnt",
-		text = "Segundos oxígeno: " .. getSegundosOxigeno()
-  })
+
 
   local done = director:createSprite({
 		x = 1000,
@@ -67,7 +155,7 @@ function crearGame()
 		yAnchor=0.5,
 		source = "sprites/play/guardar.png"
   })
-    
+   --[[ 
   local mon = director:createSprite({
     x=director.displayCenterX,
     y=director.displayCenterY,
@@ -77,7 +165,7 @@ function crearGame()
 		yAnchor=0.5,
     source = "sprites/play/mono.png"
   })
-
+--]]
   local upgrade = director:createSprite({
     x=-200,
     y=director.displayCenterY,
@@ -98,42 +186,59 @@ function crearGame()
 		source = "sprites/play/editar.png"
   })
 
-local mono = director:createSprite({
+
+local mejora = director:createSprite({
     x=400,
     y=-130,
-    xScale = 2,
-    yScale = 2,
+    xScale = 2.5,
+    yScale = 2.5,
     xAnchor=0.5,
 		yAnchor=0.5,
     block=false,
     fuera = false,
-    nombre = "edificio",
-    source = "sprites/play/edificio.png"
+    source = "sprites/play/mejorar2.png"
  })
 
 
-local lab = director:createSprite({
-    color = color.black,
-    x=700,
-    y=-130,
-    xScale = 0.3,
-    yScale = 0.3,
-    xAnchor=0.5,
-		yAnchor=0.5,
-    block=false,
-    fuera = false,
-    available = false,
-    nombre = "lab",
-    source = "sprites/play/lab.png"
- })
+sprites={oxigeno,mejora,comida,nave}
 
-sprites={mono,lab}
-function mono:touch(event)
+-- Textos
+
+	local segundosOxigenoLabel = director:createLabel({
+		x=100,
+		y=650,
+		font = "fonts/Default.fnt",
+		text = "Segundos oxígeno: " .. getSegundosOxigeno()
+  })
+
+local joyas = director:createLabel({
+		x=-370,
+		y=470,
+		font = "fonts/Default.fnt",
+		text = "Joyas: " .. joyasRecurso
+  })
+
+local precioCasa = director:createLabel({
+		x=-250,
+		y=250,
+		font = "fonts/Default.fnt",
+		text = "Precio: " .. precioCasa
+  })
+
+local precioC = director:createLabel({
+		x=-500,
+		y=250,
+		font = "fonts/Default.fnt",
+		text = "Precio: " .. precioComida
+  })
+
+function onObjectTouch(event)
   if not objetoSeleccionado then
     objetoSeleccionado = event.target
   end
   if objetoSeleccionado.block == false then
       if event.phase == "began" then
+        print(objetoSeleccionado.block)
           u = objetoSeleccionado.x
           v = objetoSeleccionado.y
           objetoSeleccionado.x = event.x
@@ -148,7 +253,6 @@ function mono:touch(event)
               if objetoSeleccionado.x+200 > b.x and objetoSeleccionado.x-120 < b.x and objetoSeleccionado.y+200 > b.y and objetoSeleccionado.y-200 < b.y then
                 objetoSeleccionado.x = u
                 objetoSeleccionado.y = v
-              else
               end
             end
           end
@@ -157,14 +261,14 @@ function mono:touch(event)
           objetoSeleccionado = nil
 
       elseif event.phase == "moved" then
-        local dx = event.x -   objetoSeleccionado.x
-        local dy = event.y -   objetoSeleccionado.y
-         objetoSeleccionado.x =   objetoSeleccionado.x + dx
-          objetoSeleccionado.y =   objetoSeleccionado.y + dy
-          objetoSeleccionado.x = event.x
-          objetoSeleccionado.y = event.y
+        local dx = event.x - objetoSeleccionado.x
+        local dy = event.y - objetoSeleccionado.y
+        objetoSeleccionado.x = objetoSeleccionado.x + dx
+        objetoSeleccionado.y = objetoSeleccionado.y + dy
+        objetoSeleccionado.x = event.x
+        objetoSeleccionado.y = event.y
       end
-      end
+    end
 end
 
  function stop(event)
@@ -182,7 +286,8 @@ end
    if event.phase == "began" then
     for a, b in pairs(sprites) do
         b.block = false
-      end      done.x = 1000
+      end
+      done.x = 1000
       editar.x = 1500
       edicion = true
     end
@@ -191,49 +296,100 @@ end
  function up(event)
    if event.phase == "began" then
      if edicion == false then
-      upgrade.x = event.target.x;
+      upgrade.y = event.target.y - 150;
+      upgrade.x = event.target.x
     end  
   end
 end
 
-
+function mejorar(event)
+  if event.phase == "began" then
+    mejora.x = oxigeno.x
+    mejora.y = oxigeno.y
+    --mono = nil
+    upgrade.x = -300
+  end
+end
 
 function menu(event)
   if event.phase == "began" then
-    if not construir.yFlip then 
-      barra.y = 100
-      if lab.fuera == false then
-        lab.y = 130
+    if not construir.xFlip then 
+      if comida.fuera == false then
+        tween:to(comida,{
+          x=550,
+        })
       end
-      if mono.fuera == false then
-        mono.y = 130
+ 
+      if oxigeno.fuera == false then
+        tween:to(oxigeno,{
+          x=250,
+        })
       end
-      construir.yFlip = true
+      tween:to(barra,{
+				x=445,
+      })
+      tween:to(joyas,{
+				x=370,
+      })
+      tween:to(precioCasa,{
+				x=250,
+      })
+      tween:to(precioC,{
+				x=500,
+      })
+      construir.xFlip = true
+
     else
-      construir.yFlip = false
-        barra.y = -200
-      if lab.fuera  == false then
-        lab.y = -130
-       end
-      if mono.fuera  == false then
-        mono.y = -130
+      construir.xFlip = false
+      if oxigeno.fuera  == false then
+        tween:to(oxigeno,{
+          x=-500,
+         })
       end
+      if comida.fuera == false then
+        tween:to(comida,{
+          x=-400,
+         })
+      end
+      tween:to(barra,{
+				x=-445,
+      }) 
+      tween:to(joyas,{
+				x=-370,
+      })
+      tween:to(precioCasa,{
+				x=-250,
+      })
+      tween:to(precioC,{
+				x=-500,
+      })
     end
   end
 end
 
 
-mono:addEventListener("touch", mono)
+--mono:addEventListener("touch",mono)
+--comida:addEventListener("touch",mono)
 done:addEventListener("touch",stop)
 editar:addEventListener("touch",edit)
-mono:addEventListener("touch",up)
---upgrade:addEventListener("touch",mejorar)
+oxigeno:addEventListener("touch",up)
+upgrade:addEventListener("touch",mejorar)
+--mejora:addEventListener("touch",mono)
+--nave.add
 
-if lab.available == true then
-  lab:addEventListener("touch", mono)
+for a,b in pairs(sprites) do
+  b:addEventListener("touch",onObjectTouch)
 end
 
+--[[
+if comida.available == true then
+  comida:addEventListener("touch", mono)
+end
+--]]
+
 construir:addEventListener("touch",menu)
+
+
 
 	function cambiarEscena(event)
 
---
+++ build_temp/deployments/default/android/release/arm/intermediate_files/src/com/carlossebastianloredogomez/JUANPI/Resources.java
@@ -0,0 +1,18 @@
+/*
+ * (C) 2001-2012 Marmalade. All Rights Reserved.
+ *
+ * This document is protected by copyright, and contains information
+ * proprietary to Marmalade.
+ *
+ * This file consists of source code released by Marmalade under
+ * the terms of the accompanying End User License Agreement (EULA).
+ * Please do not use this program/source code before you have read the
+ * EULA and have agreed to be bound by its terms.
+ */
+
+package com.ideaworks3d.marmalade;
+
+public class Resources
+{
+    public static String VFSPROVIDER_AUTHORITY = "zzzzc72fda8abb3a1f70db6428034aa08c56.VFSProvider";
+}
